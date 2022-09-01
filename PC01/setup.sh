eaa94082f46992e8bd0bfd8aa8d09f16e7949554
Trenton L. Ford <tford5@nd.edu>
2022-09-01 13:16:30 -0400

    add makefiles and correct downloader

---
+++ PC01/setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# The above line says what should execute the script if it is run
+# as an executable.
+
+# Download the C/C++ files
+wget https://raw.githubusercontent.com/tford9/CSE_20133/main/PC01/problem1.c
+wget https://raw.githubusercontent.com/tford9/CSE_20133/main/PC01/problem2.c
+wget https://raw.githubusercontent.com/tford9/CSE_20133/main/PC01/problem3.c
+wget https://raw.githubusercontent.com/tford9/CSE_20133/main/PC01/problem4.c
+wget https://raw.githubusercontent.com/tford9/CSE_20133/main/PC01/problem5.c
+wget https://raw.githubusercontent.com/tford9/CSE_20133/main/PC01/problem6.c
+wget https://raw.githubusercontent.com/tford9/CSE_20133/main/PC01/problem7.c
+wget https://raw.githubusercontent.com/tford9/CSE_20133/main/PC01/problem8.c
+wget https://raw.githubusercontent.com/tford9/CSE_20133/main/PC01/problem9.c
+wget https://raw.githubusercontent.com/tford9/CSE_20133/main/PC01/problem10.c
+
+# Download corresponding Makefile
+wget https://raw.githubusercontent.com/tford9/CSE_20133/main/PC01/Makefile
