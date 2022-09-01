cffd6bc4d5a7284f61131716818fc78966e8946d
Trenton L. Ford <tford5@nd.edu>
2022-09-01 17:41:13 -0400

    adding problem11

---
+++ PC01/problem11.c
@@ -0,0 +1,11 @@
+// Author: Trenton W. Ford
+// E-mail: tford5@nd.edu
+
+int main() {
+	int radius_one = 2;
+	float radius_two = 1.67;
+
+	// Do your programming magic
+
+	return 0;
+}
--- PC01/setup.sh
+++ PC01/setup.sh
@@ -13,6 +13,7 @@ wget https://raw.githubusercontent.com/tford9/CSE_20133/main/PC01/problem7.c
 wget https://raw.githubusercontent.com/tford9/CSE_20133/main/PC01/problem8.c
 wget https://raw.githubusercontent.com/tford9/CSE_20133/main/PC01/problem9.c
 wget https://raw.githubusercontent.com/tford9/CSE_20133/main/PC01/problem10.c
+wget https://raw.githubusercontent.com/tford9/CSE_20133/main/PC01/problem11.c
 
 # Download corresponding Makefile
 wget https://raw.githubusercontent.com/tford9/CSE_20133/main/PC01/Makefile
