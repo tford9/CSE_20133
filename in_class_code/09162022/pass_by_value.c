9aa26fd9115a1491c941847b490b8bb273ca90c9
Trenton Ford <tford5@nd.edu>
2022-09-15 22:53:21 -0400

    Making datatypes consistent

--- in_class_code/09162022/pass_by_value.c
+++ in_class_code/09162022/pass_by_value.c
@@ -10,7 +10,7 @@ int main() {
 
   // prompt user and then read their input from console
   printf("Please enter a number that you want doubled:\n");
-  scanf("%d", &user_input);
+  scanf("%f", &user_input);
   printf("The value in user_input is: %f \n", user_input);
 
   // call a function and capture what it returns
