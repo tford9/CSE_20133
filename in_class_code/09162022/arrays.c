bc76fc02a8d0e3a093a790db01b80ea490803b3f
Trenton Ford <tford5@nd.edu>
2022-09-16 00:31:08 -0400

    adding arrays

--- in_class_code/09162022/arrays.c
+++ in_class_code/09162022/arrays.c
@@ -11,6 +11,7 @@ int main() {
   // my_arr isn't actually containing data, it's a pointer to a contiguous space
   // containing numbers
   printf("%p\n", my_arr);
+  printf("In the first position of my_arr is: %d \n", my_arr[0]);
 
   return 0;
 }
