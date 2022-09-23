399520f444325f97073ff55dbdf92ce70eff7995
Trenton Ford <tford5@student10.cse.nd.edu>
2022-09-23 02:14:51 -0400

    in class version

--- in_class_code/09212022/board_initializer_testbed.c
+++ in_class_code/09212022/board_initializer_testbed.c
@@ -9,8 +9,17 @@ int main()
     int h = 40;
 
     unsigned board[w][h];
-    
+
     board_initializer(board, w, h);
+
+    for (int i = 0; i < w; i++)
+    {
+      for (int j = 0; j < h; j++)
+      {
+          printf("%d ", board[i][j]);
+      }
+      printf("\n");
+    }
 
     return 0;
 }
@@ -19,10 +28,14 @@ void board_initializer(void *b, int w,int h)
 {
     int (*empty_board)[w] = b;
 
-    
+    for (int i = 0; i < w; i++)
+    {
 
-    // now you treat empty_board like a normal array
-
-    /* try to write a function that will initialize the positions randomly */
-
+      for (int j = 0; j < h; j++)
+      {
+          int rand_one = rand() % 2;
+          printf("%d\n", rand_one);
+          empty_board[i][j] = rand_one;
+      }
+    }
 }
