6c7cb7a56e2386032fb40139f825365fe5012914
Trenton Ford <tford5@student10.cse.nd.edu>
2022-09-26 22:11:52 -0400

    gitting right with git

---
+++ in_class_code/09232022/board_initializer_testbed.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h> // included for rand() function, RAND_MAX value, and atoi() function
+
+void board_initializer(void *, int, int);
+
+int main()
+{
+    int w = 40;
+    int h = 40;
+
+    unsigned board[w][h];
+
+    board_initializer(board, w, h);
+
+    for (int i = 0; i < w; i++)
+    {
+      for (int j = 0; j < h; j++)
+      {
+          printf("%d ", board[i][j]);
+      }
+      printf("\n");
+    }
+
+    return 0;
+}
+
+void board_initializer(void *b, int w,int h)
+{
+    int (*empty_board)[w] = b;
+
+    for (int i = 0; i < w; i++)
+    {
+
+      for (int j = 0; j < h; j++)
+      {
+          int rand_tenth = rand() % 10;
+          // printf("%d\n", rand_tenth);
+          if(rand_tenth == 0){
+              empty_board[i][j] = 1;
+          } else 
+          {
+             empty_board[i][j] = 0;
+          }
+
+      }
+    }
+}
---
+++ in_class_code/09232022/evolve_testbed.c
@@ -0,0 +1,90 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void show(void *, int, int);
+void board_initializer(void *,int,int);
+void evolve_board(void *,int,int);
+
+int main()
+{
+    int w = 40;
+    int h = 40;
+
+    unsigned int board[w][h];
+    board_initializer(board, w, h);
+    while(1)
+    {
+      show(board, w, h);
+    }
+
+}
+
+void evolve_board(void *,int,int)
+int (*current_board)[w] = b;
+
+for(int i = 0; i < w; i++)
+{
+  for(int j = 0; j < h; j++)
+  {
+    if (i != w-1 && i != 0 && j !=h-1 && j != 0)
+    {
+      for (x = i - 1, y = j - 1; y++,x++)
+      {
+
+      }
+    }
+  }
+}
+
+}
+
+void show(void *b, int w, int h)
+{
+
+    int (*current_board)[w] = b;
+
+    printf("\033[0;0H");
+    printf("\033[2J");
+
+    for(int i = 0; i < w; i++)
+    {
+      for(int j = 0; j < h; j++)
+      {
+        if(current_board[i][j]==0)
+        {
+          printf("  ");
+        } else
+        {
+          printf("\033[7m  \033[27m");
+        }
+      }
+      printf("\n");
+    }
+    printf("\033[E");
+
+    /* your code goes here print out each value in each cell in a grid */
+
+    fflush(stdout);
+}
+
+void board_initializer(void *b, int w,int h)
+{
+    int (*empty_board)[w] = b;
+
+    for (int i = 0; i < w; i++)
+    {
+
+      for (int j = 0; j < h; j++)
+      {
+          int rand_tenth = rand() % 10;
+          // printf("%d\n", rand_tenth);
+          if(rand_tenth == 0){
+              empty_board[i][j] = 1;
+          } else
+          {
+             empty_board[i][j] = 0;
+          }
+
+      }
+    }
+}
