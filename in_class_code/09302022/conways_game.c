25fb1e79ee6c3fa175b549cad68096d740cb7884
Trenton Ford <tford5@student10.cse.nd.edu>
2022-09-29 20:07:46 -0400

    adding makefile test stuff "

---
+++ in_class_code/09302022/conways_game.c
@@ -0,0 +1,10 @@
+#include "conways_game_utilities.h"
+
+int main()
+{
+    unsigned int w = 40;
+    unsigned int h = 40;
+
+    game_runner(w,h);
+    return 0;
+}
---
+++ in_class_code/09302022/conways_game_utilities.h
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <unistd.h>
+
+int game_runner(unsigned int,unsigned int);
