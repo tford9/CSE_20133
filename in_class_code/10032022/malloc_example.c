061fc73ced6bdeea6fa57c928216b09f90d37892
Trenton Ford <tford5@student10.cse.nd.edu>
2022-10-03 10:44:12 -0400

    I'm old

--- in_class_code/10032022/malloc_example.c
+++ in_class_code/10032022/malloc_example.c
@@ -3,18 +3,49 @@
 //
 
 #include <stdio.h>
-
+#include <stdlib.h>
 
 int global_var = 14; // this variable is stored in the "data" portion of the program memory
 
+int my_other_global;
+
 int main()
 {
+    //
+    // char name[] = "CSE 20133"; // this variable is stored on the stack at the start of runtime
+    // /*  */
+    //
+    // char *future_string = (*char)malloc(sizeof(char)*10);
+    //
+    // char *other_name = (*char)future_string;
 
-    char name[] = "CSE 20133"; // this variable is stored on the stack at the start of runtime
-    /*  */
-    char *other_name;
+    char char_arr[256];
 
-    other_name = (*char)malloc(sizeof(char)*10);
+    fgets(char_arr, 256, stdin);
+
+    char * slim_arr;
+
+    int length = 0;
+    for(int i = 0; i < sizeof(char_arr)/sizeof(char); i++)
+    {
+      if (char_arr[i] == '\0')
+        {
+          break;
+        } else {
+          length+=1;
+        }
+    }
+
+    slim_arr = (char*)malloc(sizeof(char)*(length+1));
 
 
+    for (int i = 0; i<length; i++)
+    {
+      slim_arr[i] = char_arr[i];
+    }
+    slim_arr[length-1] = '\0';
+
+    printf("%s is %d characters long.", slim_arr, length);
+
+    return 0;
 }
