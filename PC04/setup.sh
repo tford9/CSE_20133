1e9340a2b86f15b11dd3e23491e75a6264befabf
Trenton Ford <tford5@nd.edu>
2022-10-06 22:01:59 -0400

    pc04 starter

---
+++ PC04/setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# The above line says what should execute the script if it is run
+# as an executable.
+
+# Download the C/C++ files
+wget https://raw.githubusercontent.com/tford9/CSE_20133/main/PC04/Makefile
