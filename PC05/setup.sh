ef59694b171c4c18287e1f75cb9a8200f7ad568c
Trenton Ford <tford5@nd.edu>
2022-10-16 02:26:43 -0400

    update setup.sh

---
+++ PC05/setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# The above line says what should execute the script if it is run
+# as an executable.
+
+# Download the C/C++ files
+wget https://raw.githubusercontent.com/tford9/CSE_20133/main/PC05/problem1/sorter.c
+wget https://raw.githubusercontent.com/tford9/CSE_20133/main/PC05/problem1/sorter.h
+wget https://raw.githubusercontent.com/tford9/CSE_20133/main/PC05/problem2/problem2.c
+
